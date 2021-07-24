cb0d497c763d15dc53b6951b3b6f474f2a697e33
Mary Smith <mary.sunshine4@gmail.com>
2021-07-24 01:27:07 +0000

    Fixed range. Add play again and tracker

--- game.py
+++ game.py
@@ -3,29 +3,42 @@
 # Put your code here
 from random import randint
 
-secret_num = randint(1,101)
 
 name = input("Howdy! What is your name: ").title()
 print(f"{name}, I'm thinking of a number between 1 and 100.")
 print("Try to guess my number.")
-
-num_of_guesses = 0
+guesses_list = []
 
 while True:
-    try:
-        guess = int(input("Your guess? "))
-    
 
-        if guess < 1 or guess > 100:
-            print("Guess between 1 and 100.")
-        else:
-            num_of_guesses += 1
-            if guess > secret_num:
-                print("Your guess is too high -- try again.")
-            elif guess < secret_num:
-                print("Your guess is too low -- try again.")
+    secret_num = randint(1,100)
+
+
+    num_of_guesses = 0
+
+    while True:
+        try:
+            guess = int(input("Your guess? "))
+        
+
+            if guess < 1 or guess > 100:
+                print("Guess between 1 and 100.")
             else:
-                print(f"Well done, {name}! You found my number in {num_of_guesses} tries!")
-                break 
-    except ValueError:
-        print("Oops! The was not a number. Try again.")   
+                num_of_guesses += 1
+                if guess > secret_num:
+                    print("Your guess is too high -- try again.")
+                elif guess < secret_num:
+                    print("Your guess is too low -- try again.")
+                else:
+                    print(f"Well done, {name}! You found my number in {num_of_guesses} tries!")
+                    guesses_list.append(num_of_guesses)
+                    break 
+        except ValueError:
+            print("Oops! The was not a number. Try again.")
+
+    print(f"Good game! Your best game so far was {min(guesses_list)} guesses.")
+    play_again = input("Would you like to play again? Y or N: ").upper()
+    if 'N' in play_again:
+        print("Goodbye.")
+        break
+    
