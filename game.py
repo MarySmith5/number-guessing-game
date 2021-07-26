ab87ac3797b8cfee40899be6df7bdddbe2b78ad5
Mary Smith <mary.sunshine4@gmail.com>
2021-07-26 12:48:38 +0000

    limit number of tries

--- game.py
+++ game.py
@@ -2,20 +2,10 @@
 #print("I did it!")
 # Put your code here
 from random import randint
+    
 
-
-name = input("Howdy! What is your name: ").title()
-print(f"{name}, I'm thinking of a number between 1 and 100.")
-print("Try to guess my number.")
-guesses_list = []
-
-while True:
-
-    secret_num = randint(1,100)
-
-
+def play(rand_int, player):
     num_of_guesses = 0
-
     while True:
         try:
             guess = int(input("Your guess? "))
@@ -25,20 +15,50 @@ while True:
                 print("Guess between 1 and 100.")
             else:
                 num_of_guesses += 1
-                if guess > secret_num:
+                if num_of_guesses == 8:
+                    print('That was your last guess...')
+                    if guess != rand_int:
+                        print('Sorry. You failed this round.')
+                        return "fail"
+                if guess > rand_int:
                     print("Your guess is too high -- try again.")
-                elif guess < secret_num:
+                elif guess < rand_int:
                     print("Your guess is too low -- try again.")
                 else:
-                    print(f"Well done, {name}! You found my number in {num_of_guesses} tries!")
-                    guesses_list.append(num_of_guesses)
-                    break 
+                    print(f"Well done, {player}! You found my number in {num_of_guesses} tries!")
+                    return num_of_guesses
+                    
         except ValueError:
             print("Oops! The was not a number. Try again.")
 
-    print(f"Good game! Your best game so far was {min(guesses_list)} guesses.")
-    play_again = input("Would you like to play again? Y or N: ").upper()
-    if 'N' in play_again:
-        print("Goodbye.")
-        break
-    
+            
+                
+    return num_of_guesses
+
+
+def main_play():
+
+    name = input("Howdy! What is your name: ").title()
+    print(f"{name}, I'm thinking of a number between 1 and 100.")
+    print("Try to guess my number in 8 or fewer guesses.")
+    guesses_list = []
+
+    while True:
+
+        secret_num = randint(1,100)
+        outcome = play(secret_num, name)
+        if outcome != 'fail':
+            guesses_list.append(outcome)
+
+        if guesses_list != []:
+
+            print(f"Your best game so far was {min(guesses_list)} guesses.")
+        play_again = input("Would you like to play again? Y or N: ").upper()
+        if 'N' in play_again:
+            print("Goodbye.")
+            break
+
+main_play()
+
+
+        
